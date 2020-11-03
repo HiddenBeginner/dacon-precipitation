ef8ca30aafd1d03cbdc84515d43808dc153211b9
HiddenBeginner <39156266+HiddenBeginner@users.noreply.github.com>
2020-11-03 15:07:59 +0900

    Transpose dimensions from (batch_size,H,W,C) to (batch_size,C,H,W)

--- datasets.py
+++ datasets.py
@@ -1,5 +1,8 @@
 import numpy as np
+
+import torch
 from torch.utils.data import Dataset
+
 
 class PrecipitationDataset(Dataset):
     def __init__(self, df, dir_sample, resize, mode, transform):
@@ -19,18 +22,20 @@ class PrecipitationDataset(Dataset):
         sample_id = self.df.loc[index, 'id']
 
         X, y = self.load_sample_and_label(sample_id)
+        X = torch.as_tensor(X).permute(2, 0, 1)
+        y = torch.as_tensor(y).permute(2, 0, 1)
 
-        return X, y
+        return X, y, sample_id
 
     def load_sample_and_label(self, sample_id):
         sample = np.load(f'{self.dir_sample}/{sample_id}').astype(np.float32)
         if self.mode == 'test':
-            sample = np.concatenate((sample, np.zeros((self.resize, self.resize, 1))), axis=-1)
+            sample = np.concatenate((sample, np.zeros((self.resize, self.resize, 1), dtype=np.float32)), axis=-1)
 
         sample = sample / 255.
 
         X = sample[:, :, :4]
-        y = sample[:, :, 4]
+        y = sample[:, :, 4:5]
 
         return X, y
 
