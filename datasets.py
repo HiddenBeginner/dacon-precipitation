00a0a2d38cb23152374dbd803d89f9a7fa97224f
HiddenBeginner <39156266+HiddenBeginner@users.noreply.github.com>
2020-11-04 20:33:25 +0900

    feat: Add CutMix augmentation

--- datasets.py
+++ datasets.py
@@ -5,10 +5,10 @@ from torch.utils.data import Dataset
 
 
 class PrecipitationDataset(Dataset):
-    def __init__(self, df, dir_sample, resize, mode, transform):
+    def __init__(self, df, dir_sample, imsize, mode, transform):
         self.df = df
         self.dir_sample = dir_sample
-        self.resize = resize
+        self.imsize = imsize
         self.mode = mode
         self.transform = transform
 
@@ -21,16 +21,21 @@ class PrecipitationDataset(Dataset):
     def __getitem__(self, index):
         sample_id = self.df.loc[index, 'id']
 
-        X, y = self.load_sample_and_label(sample_id)
+        if self.mode == 'test' or np.random.random() > 0.5:
+            X, y = self.load_sample_and_label(index)
+        else:
+            X, y = self.load_cutmix_sample_and_label(index, imsize=self.imsize)
+
         X = torch.as_tensor(X).permute(2, 0, 1)
         y = torch.as_tensor(y).permute(2, 0, 1)
 
         return X, y, sample_id
 
-    def load_sample_and_label(self, sample_id):
+    def load_sample_and_label(self, index):
+        sample_id = self.df.loc[index, 'id']
         sample = np.load(f'{self.dir_sample}/{sample_id}').astype(np.float32)
         if self.mode == 'test':
-            sample = np.concatenate((sample, np.zeros((self.resize, self.resize, 1), dtype=np.float32)), axis=-1)
+            sample = np.concatenate((sample, np.zeros((self.imsize, self.imsize, 1), dtype=np.float32)), axis=-1)
 
         sample = sample / 255.
 
@@ -38,4 +43,40 @@ class PrecipitationDataset(Dataset):
         y = sample[:, :, 4:5]
 
         return X, y
+
+    def load_cutmix_sample_and_label(self, index, imsize):
+        """
+        This implementation of cutmix author:  https://www.kaggle.com/nvnnghia
+        Refactoring and adaptation: https://www.kaggle.com/shonenkov
+        """
+        w, h = imsize, imsize
+        s = imsize // 2
+
+        xc, yc = [int(np.random.uniform(imsize*0.25, imsize*0.75)) for _ in range(2)]
+        # Choose three random images
+        indices = [index] + [np.random.randint(0, self.__len__() - 1) for _ in range(3)]
+
+        # Initialize the result image
+        result_X = np.full((w, h, 4), 1, dtype=np.float32)
+        result_y = np.full((w, h, 1), 1, dtype=np.float32)
+
+        for i, index in enumerate(indices):
+            X, y = self.load_sample_and_label(index)
+
+            if i == 0:
+                x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # top left
+            elif i == 1:
+                x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc  # top right
+            elif i == 2:
+                x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)  # bottom left
+            elif i == 3:
+                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)  # bottom right
+
+            result_X[y1a:y2a, x1a:x2a] = X[y1a:y2a, x1a:x2a]
+            result_y[y1a:y2a, x1a:x2a] = y[y1a:y2a, x1a:x2a]
+
+        return result_X, result_y
+
+
+
 
